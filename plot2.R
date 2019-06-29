cf3bcf93142990db992d80d3b428cbf2983c2b89
SakshiSrivastava <sakshi.srivastava2106@gmail.com>
2019-06-29 17:59:57 +0530

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,37 @@
+plot2 <- function(){ 
+  ## Purpose of this function is -
+  ## 1. Read Data from household_power_consumption.txt
+  ## 2. Get Data specific to Date - 2007-02-01 & 2007-02-02
+  ## 3. Generate a Graph based on Global Active Power vs Date and Time
+  
+  ## Read Data  
+  source_data <- read.table("household_power_consumption.txt",stringsAsFactors = FALSE,header = TRUE,sep = ";")
+  
+  ##Add Date and Time column in the Source data, merged together as DateTime
+  
+  TimeDate <- strptime(paste(source_data$Date, source_data$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+  source_data <- cbind(source_data, TimeDate)
+  
+  ## Convert class of all fields as numeric and datetime format specific
+  
+  source_data$Date <- as.Date(source_data$Date,format = "%d/%m/%Y")
+  source_data$Time <- format(source_data$Time,format = "%H:%M:%S:")
+  source_data$Global_active_power <- as.numeric(source_data$Global_active_power)
+  source_data$Global_reactive_power <- as.numeric(source_data$Global_reactive_power)
+  source_data$Voltage <- as.numeric(source_data$Voltage)
+  source_data$Global_intensity <- as.numeric(source_data$Global_intensity)
+  source_data$Sub_metering_1 <- as.numeric(source_data$Sub_metering_1)
+  source_data$Sub_metering_2 <- as.numeric(source_data$Sub_metering_2)
+  source_data$Sub_metering_3 <- as.numeric(source_data$Sub_metering_3)
+  
+  ##Get Data only for the specific Dates
+  
+  data_subset <- subset(source_data, Date == "2007-02-01" | Date == "2007-02-02")
+  
+  ## Plot a Graph based on Global Active Power Vs. Date and Time
+  
+  png("plot2.png", width=480, height=480)
+  with(data_subset, plot(TimeDate, Global_active_power, type="l", xlab="Day", ylab="Global Active Power (kilowatts)"))
+  dev.off()
+  
+}
